9d307d42e833e42f7356016acc98ee4162018474
ddstupin2019 <91018296+ddstupin2019@users.noreply.github.com>
2022-04-17 20:56:58 +0300

    t
    t

---
+++ src/main/java/com/example/Prodanogame/ProdanogameApplication.java
@@ -0,0 +1,13 @@
+package com.example.Prodanogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdanogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdanogameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Prodanogame/ProdanogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prodanogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdanogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
